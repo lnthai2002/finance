a5a385cbb1b8b66f655165d3f2695e74b1a87cfb
Thai Le <lnthai2002@gmail.com>
2020-10-06 15:48:19 -0400

    base CRUD repository using Map. Close #7

---
+++ finance-data/src/main/java/repository/map/MapCrudRepository.java
@@ -0,0 +1,33 @@
+package repository.map;
+
+import repository.CrudRepository;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public abstract class MapCrudRepository<T> {
+    private Map<Long, T> map = new HashMap<>();
+
+    public T save(Long id, T object) {
+        map.put(id, object);
+        return object;
+    }
+
+    public Set<T> findAll() {
+        return new HashSet<>(map.values());
+    }
+
+    public T findById(Long id) {
+        return map.get(id);
+    }
+
+    public void delete(T object) {
+        map.entrySet().removeIf(pair -> pair.getValue().equals(object));
+    }
+
+    public void deleteById(Long id) {
+        map.remove(id);
+    }
+}
