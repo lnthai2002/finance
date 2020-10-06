d5213decf493cb758d03e90689f521df1bde7244
Thai Le <lnthai2002@gmail.com>
2020-10-06 13:19:53 -0400

    initial commit. Already break master pom into data and web module

---
+++ finance-web/src/main/java/info/noip/darkportal/finance/web/FinanceApplication.java
@@ -0,0 +1,13 @@
+package info.noip.darkportal.finance.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceApplication.class, args);
+	}
+
+}
