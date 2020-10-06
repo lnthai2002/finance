5ce6996ffe9c2067f1a84269525fec2cadb66094
Thai Le <lnthai2002@gmail.com>
2020-10-06 15:16:24 -0400

    CRUD repository interface. Close #6

---
+++ finance-data/src/main/java/repository/CrudRepository.java
@@ -0,0 +1,11 @@
+package repository;
+
+import java.util.Set;
+
+public interface CrudRepository<T> {
+    T save(T object);
+    Set<T> findAll();
+    T findById(Long id);
+    void delete(T object);
+    void deleteById(Long id);
+}
