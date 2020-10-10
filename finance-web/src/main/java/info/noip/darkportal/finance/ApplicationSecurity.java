cd31efe01a26109b1d840a401b28a170979715c7
Thai Le <lnthai2002@gmail.com>
2020-10-10 11:22:54 -0400

    Disable security, for now. Close #16

---
+++ finance-web/src/main/java/info/noip/darkportal/finance/ApplicationSecurity.java
@@ -0,0 +1,15 @@
+package info.noip.darkportal.finance;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Profile("mapdata")
+@Configuration
+public class ApplicationSecurity extends WebSecurityConfigurerAdapter {
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/**");
+    }
+}
