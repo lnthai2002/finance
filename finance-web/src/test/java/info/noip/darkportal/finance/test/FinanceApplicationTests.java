e579fd96dc69d483c0b8f12077b8d69e90d68ced
Thai Le <lnthai2002@gmail.com>
2020-10-10 14:19:43 -0400

    move test class to the test package right under finance package to match the Application. Also need ti supply the Application class that the test uses. See https://stackoverflow.com/a/39084906 Close #18

---
+++ finance-web/src/test/java/info/noip/darkportal/finance/test/FinanceApplicationTests.java
@@ -0,0 +1,14 @@
+package info.noip.darkportal.finance.test;
+
+import info.noip.darkportal.finance.FinanceApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = FinanceApplication.class)
+public class FinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
