c4ebb8f67054d09d90d1378a5528b49c37ce3f41
Thai Le <lnthai2002@gmail.com>
2020-10-20 13:41:58 -0400

    payment controller that return all payments. Part of #15

---
+++ finance.web/src/main/java/info/noip/darkportal/finance/web/controller/PaymentController.java
@@ -0,0 +1,26 @@
+package info.noip.darkportal.finance.web.controller;
+
+import info.noip.darkportal.finance.data.model.Payment;
+import info.noip.darkportal.finance.data.repository.PaymentRepository;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequestMapping("/payments")
+@RestController
+public class PaymentController {
+    private PaymentRepository paymentRepository;
+
+    public PaymentController(PaymentRepository paymentRepository) {
+        this.paymentRepository = paymentRepository;
+    }
+
+    @GetMapping({"","/"})
+    public List<Payment> getPayments(Model model) {
+        List<Payment> payments = paymentRepository.findAll();
+        return payments;
+    }
+}
