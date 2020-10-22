b69745f01c561a660f499b2f2121ca60e14569f0
Thai Le <lnthai2002@gmail.com>
2020-10-22 16:44:22 -0400

    handle EntityNotFoundException and return error code to conform with REST. Close #15

---
+++ finance.web/src/main/java/info/noip/darkportal/finance/web/controller/RestExceptionHandler.java
@@ -0,0 +1,28 @@
+package info.noip.darkportal.finance.web.controller;
+
+import info.noip.darkportal.finance.data.repository.EntityNotFoundException;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
+@Order(Ordered.HIGHEST_PRECEDENCE)
+@ControllerAdvice
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+
+    private ResponseEntity<Object> buildResponseEntity(ApiError apiError) {
+        return new ResponseEntity<Object>(apiError, apiError.getStatus());
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    protected ResponseEntity<Object> handleEntityNotFound(
+            EntityNotFoundException ex) {
+        ApiError apiError = new ApiError(NOT_FOUND);
+        apiError.setMessage(ex.getMessage());
+        return buildResponseEntity(apiError);
+    }
+}
