881c01f960494a01cad95d8524ac706f00f26486
Thai Le <lnthai2002@gmail.com>
2020-10-22 16:42:07 -0400

    add method to get specific payment belong to a person. Part of #15

--- finance.web/src/main/java/info/noip/darkportal/finance/web/controller/PaymentController.java
+++ finance.web/src/main/java/info/noip/darkportal/finance/web/controller/PaymentController.java
@@ -1,6 +1,8 @@
 package info.noip.darkportal.finance.web.controller;
 
+import info.noip.darkportal.finance.data.Messages;
 import info.noip.darkportal.finance.data.model.Payment;
+import info.noip.darkportal.finance.data.repository.EntityNotFoundException;
 import info.noip.darkportal.finance.data.service.PaymentService;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,14 +12,25 @@ import java.util.List;
 @RestController
 public class PaymentController {
     private PaymentService paymentService;
+    private Messages messages;
 
-    public PaymentController(PaymentService paymentService) {
+    public PaymentController(PaymentService paymentService, Messages messages) {
         this.paymentService = paymentService;
+        this.messages = messages;
     }
 
     @GetMapping({"","/"})
     public List<Payment> getPayments(@RequestParam Long personId) {
         List<Payment> payments = paymentService.findAll(personId);
         return payments;
+    }
+
+    @GetMapping("/{id}")
+    public Payment getPayment(@PathVariable Long id, @RequestParam Long personId) {
+        Payment payment = paymentService.findById(id);
+        if (payment.getPerson().getId() != personId) {
+            throw new EntityNotFoundException(messages.get("jpa.entityNotFound"));
+        }
+        return payment;
     }
 }
