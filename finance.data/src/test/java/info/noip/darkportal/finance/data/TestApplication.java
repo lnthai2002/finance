2bb9741e8a90ec994bd1b917f983550a2b715e2c
Thai Le <lnthai2002@gmail.com>
2020-10-28 15:25:13 -0400

    Fix error that SpringConfiguration is not found.

---
+++ finance.data/src/test/java/info/noip/darkportal/finance/data/TestApplication.java
@@ -0,0 +1,10 @@
+package info.noip.darkportal.finance.data;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * The sole purpose of this class is to make the all the tests found @SpringBootConfiguration
+ * see: https://www.baeldung.com/spring-boot-unable-to-find-springbootconfiguration-with-datajpatest*/
+@SpringBootApplication
+public class TestApplication {
+}
